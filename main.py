e5790e574f8decaf8ac305b84776ddfab60705fa
Vincent-Lien <vincentlien0606@gmail.com>
2024-10-19 19:51:17 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,127 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def play_game(stg):
+    # Initialize WebDriver
+    driver = webdriver.Chrome()
+
+    # Open browser
+    driver.get("http://203.66.45.207:8000/play/mzb")
+
+    # close-btn
+    try:
+        close_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "close-btn"))
+        )
+        close_btn.click()
+    except Exception as e:
+        print(f"Could not find the close-btn: {e}")
+
+    time.sleep(3)
+
+    # Go to the next page if the stage is not in the page (15 stages in a page)
+    for _ in range((stg - 1) // 15):
+        # right-btn
+        try:
+            right_btn = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="right-btn"]'))
+            )
+            right_btn.click()
+        except Exception as e:
+            print(f"Could not find the right-btn: {e}")
+        time.sleep(1)
+
+    # Enter the stage
+    try:
+        ep_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    f"//button[contains(@class, 'stage') and text()='EP{stg}']",
+                )
+            )
+        )
+        ep_btn.click()
+    except Exception as e:
+        print(f"Could not find the EP{stg}: {e}")
+
+    # audio-btn
+    try:
+        audio_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "audio-btn"))
+        )
+        audio_btn.click()
+    except Exception as e:
+        print(f"Could not find the audio-btn: {e}")
+
+    time.sleep(5)
+
+    # start-btn
+    try:
+        start_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "start-btn"))
+        )
+        start_btn.click()
+    except Exception as e:
+        print(f"Could not find the start-btn: {e}")
+
+    time.sleep(3)
+
+    # Check and click all button with the feature of is-correct="true"
+    try:
+        for stage in range(1, 13):
+            time.sleep(0.4)  # To avoid getting 1st place everytime
+            card = driver.find_element(By.ID, f"card{stage}")
+            if card.get_attribute("is-correct") == "true":
+                card.click()
+                print(f"Clicked: card{stage}")
+    except Exception as e:
+        print(f"Error while clicking answers: {e}")
+
+    time.sleep(5)
+
+    # Agree
+    try:
+        agree_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="tos2"]/button[2]'))
+        )
+        agree_btn.click()
+    except Exception as e:
+        print(f"Could not find the agree button: {e}")
+
+    time.sleep(3)
+
+    # Name
+    try:
+        name_block = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="name"]'))
+        )
+        name_block.send_keys("YOUR_NAME")  # Change your name here
+    except Exception as e:
+        print(f"Could not find the name: {e}")
+
+    # send button
+    try:
+        name_block = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//*[@id="leaderboard"]/div[3]/button[2]')
+            )
+        )
+        name_block.click()
+    except Exception as e:
+        print(f"Could not find the send button: {e}")
+
+    time.sleep(3)
+
+    # turn off the browser
+    driver.quit()
+
+
+if __name__ == "__main__":
+    stage_list = [7]  # Enter the stage you want to run
+    for i in stage_list:
+        play_game(i)
